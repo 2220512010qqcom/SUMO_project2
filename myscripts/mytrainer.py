4409de086c6da79445359e22a23299f5578fb67d
Li Jiaxu <2220512010@qq.com>
2026-04-27 08:24:06 +0000

    添加急救车

--- myscripts/mytrainer.py
+++ myscripts/mytrainer.py
@@ -23,7 +23,7 @@ class mytrainer:
 
     def __init__(self):
         self.episode = 0
-        self.max_episodes = 100          # 最大训练回合数
+        self.max_episodes = 5          # 最大训练回合数
         self.step_per_episode = 800     # 每个回合的最大步数
 
         self.plot_dir = './outputs/output2'
